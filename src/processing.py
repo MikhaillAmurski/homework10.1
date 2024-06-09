4030fe406345826ce74f03c59e29e0507a78e544
Amyrski <vivisnext007786@gmail.com>
2024-06-09 21:46:02 +1000

    add processing.py and fixed

---
+++ src/processing.py
@@ -0,0 +1,32 @@
+from . widget import get_data
+
+
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+    """Функция фильтрации операций по ключу state"""
+    new_list = []
+    for i in list_dict:
+        for key, value in i.items():
+            if value == state:
+                new_list.append(i)
+    return new_list
+
+
+def sort_by_date(list_dict: list, user_rev: bool = True) -> list:
+    """Функция сортировки операций по дате"""
+    if user_rev is True:
+        sorted_list = sorted(list_dict, key=lambda x: get_data(x["date"]))
+        return sorted_list
+    else:
+        sorted_list = sorted(list_dict, key=lambda x: get_data(x["date"]), reverse=False)
+        return sorted_list
+
+
+test = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+result = sort_by_date(test, False)
+print(result)
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from masks import account_number_hider, card_number_hider
+from . masks import account_number_hider, card_number_hider
 
 
 def mask_account_card(info_account_card: str) -> str:
@@ -19,13 +19,3 @@ def mask_account_card(info_account_card: str) -> str:
 def get_data(info_of_date: str) -> str:
     """Функция для преобразования даты"""
     return f"{info_of_date[8:10]}.{info_of_date[5:7]}.{info_of_date[0:4]}"
-
-
-result3 = get_data("2018-07-11T02:26:18.671407")
-result2 = mask_account_card("Visa Gold 5999414228426353")
-result1 = mask_account_card("Счет 64686473678894779589")
-result = mask_account_card("Visa Platinum 8990922113665229")
-print(result)
-print(result1)
-print(result2)
-print(result3)
