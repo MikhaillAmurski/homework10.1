d9db3d5a38ebf83179bc63707fcc2ff81131d6e0
Amyrski <vivisnext007786@gmail.com>
2024-06-09 23:12:52 +1000

    end work

--- src/processing.py
+++ src/processing.py
@@ -20,13 +20,3 @@ def sort_by_date(list_dict: list, user_rev: bool = True) -> list:
         sorted_list = sorted(list_dict, key=lambda x: get_data(x["date"]), reverse=False)
         return sorted_list
 
-
-test = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-result = sort_by_date(test, False)
-print(result)
