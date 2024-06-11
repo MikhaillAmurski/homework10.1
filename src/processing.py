1ac168d687d56166b5c8bca0896177e8ac24614f
Amyrski <vivisnext007786@gmail.com>
2024-06-12 01:01:14 +1000

    end work

--- src/processing.py
+++ src/processing.py
@@ -19,3 +19,12 @@ def sort_by_date(list_dict: list, user_rev: bool = True) -> list:
     else:
         sorted_list = sorted(list_dict, key=lambda x: get_data(x["date"]), reverse=False)
         return sorted_list
+
+
+r = (sort_by_date([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ], False))
+print(r)
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,19 @@
-def test_filter_by_state():
-    pass
+from src.processing import filter_by_state, sort_by_date
 
 
-def test_sort_by_date():
-    pass
+def test_filter_by_state(info_bank):
+    assert filter_by_state([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]) == info_bank
+
+
+def test_sort_by_date(sorted_info):
+    assert sort_by_date([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ], False) == sorted_info
